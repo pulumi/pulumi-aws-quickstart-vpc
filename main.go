589696a924491051307afc370428b8ddb19fac17
ajhool <ajhoolachan21@gmail.com>
2021-09-10 13:04:54 -0400

    Parameterized VPC configurations

--- main.go
+++ main.go
@@ -7,23 +7,67 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		vpcName := "Pulumi VPC"
 
+		/************************************
+		 * Namespace parameters
+		 ************************************/
+		vpcName := "Pulumi VPC"
 		namespace := "pulumi-vpc"
 
-		// @fixme - not implemented yet. The main challenge is the false choice.
-		// createPublicSubnets := true
+		/************************************
+		 * Network parameters
+		 ************************************/
 
+		// default || dedicated
+		vpcInstanceTenancy := "default"
+
+		// @fixme - not implemented yet. The main challenge is the false choice.
+		createPublicSubnets := true
 		createPrivateSubnets := true
 		createAdditionalPrivateSubnets := true
 
 		// @fixme - not implemented yet. The main challenge is actually the "false" choice.
-		// createNatGateways := true
+		createNatGateways := false
 
 		// VPC block
-		cidrBlock := "10.0.0.0/16"
+		vpcCidrBlock := "10.0.0.0/16"
+
+		// Add a public subnet
+		publicSubnet1Name := "Pulumi Public Subnet 1"
+		publicSubnet1Cidr := "10.0.5.0/24" // Add a public subnet
+		privateSubnet1AName := "Pulumi Private Subnet 1A"
+		privateSubnet1ACidr := "10.0.7.0/24"
+		privateSubnet1BName := "Pulumi Private Subnet 1B"
+		privateSubnet1BCidr := "10.0.224.0/21"
+
+		publicSubnet2Name := "Pulumi Public Subnet 2"
+		publicSubnet2Cidr := "10.0.8.0/24"
+		privateSubnet2AName := "Pulumi Private Subnet 2A"
+		privateSubnet2ACidr := "10.0.9.0/24"
+		privateSubnet2BName := "Pulumi Private Subnet 1A"
+		privateSubnet2BCidr := "10.0.232.0/21"
+
+		publicSubnet3Name := "Pulumi Public Subnet 3"
+		publicSubnet3Cidr := "10.0.10.0/24"
+		privateSubnet3AName := "Pulumi Private Subnet 3A"
+		privateSubnet3ACidr := "10.0.11.0/24"
+		privateSubnet3BName := "Pulumi Private Subnet 3B"
+		privateSubnet3BCidr := "10.0.240.0/21"
+
+		publicSubnet4Name := "Pulumi Public Subnet 4"
+		publicSubnet4Cidr := "10.0.12.0/24"
+		privateSubnet4AName := "Pulumi Private Subnet 4A"
+		privateSubnet4ACidr := "10.0.13.0/24"
+		privateSubnet4BName := "Pulumi Private Subnet 4B"
+		privateSubnet4BCidr := "10.0.216.0/21"
+
+		// END PARAMETERS
+
 		vpc, vpcErr := ec2.NewVpc(ctx, "vpc", &ec2.VpcArgs{
-			CidrBlock: pulumi.String(cidrBlock),
+			CidrBlock:          pulumi.String(vpcCidrBlock),
+			InstanceTenancy:    pulumi.String(vpcInstanceTenancy),
+			EnableDnsHostnames: pulumi.Bool(true),
+			EnableDnsSupport:   pulumi.Bool(true),
 			Tags: pulumi.StringMap{
 				"Name": pulumi.String(vpcName),
 			},
@@ -35,7 +79,7 @@ func main() {
 
 		// @fixme - only create the internet gateway if public subnets are allowed.
 		internetGatewayName := "Pulumi Internet Gateway"
-		internetGateway, internetGatewayErr := ec2.NewInternetGateway(ctx, "internet-gateway", &ec2.InternetGatewayArgs{
+		internetGateway, internetGatewayErr := ec2.NewInternetGateway(ctx, resourceName(namespace, "internet-gateway"), &ec2.InternetGatewayArgs{
 			VpcId: vpc.ID(),
 			Tags: pulumi.StringMap{
 				"Name": pulumi.String(internetGatewayName),
@@ -46,46 +90,17 @@ func main() {
 			return internetGatewayErr
 		}
 
-		// Add a public subnet
-		publicSubnet1Name := "Pulumi Public Subnet 1"
-		publicSubnet1Cidr := "10.0.5.0/24" // Add a public subnet
-		privateSubnet1AName := "Pulumi Private Subnet 1A"
-		privateSubnet1ACidr := "10.0.7.0/24"
-		privateSubnet1BName := "Pulumi Private Subnet 1B"
-		privateSubnet1BCidr := "10.0.14.0/24"
-
-		publicSubnet2Name := "Pulumi Public Subnet 2"
-		publicSubnet2Cidr := "10.0.8.0/24"
-		privateSubnet2AName := "Pulumi Private Subnet 2A"
-		privateSubnet2ACidr := "10.0.9.0/24"
-		privateSubnet2BName := "Pulumi Private Subnet 1A"
-		privateSubnet2BCidr := "10.0.15.0/24"
-
-		publicSubnet3Name := "Pulumi Public Subnet 3"
-		publicSubnet3Cidr := "10.0.10.0/24"
-		privateSubnet3AName := "Pulumi Private Subnet 3A"
-		privateSubnet3ACidr := "10.0.11.0/24"
-		privateSubnet3BName := "Pulumi Private Subnet 3B"
-		privateSubnet3BCidr := "10.0.16.0/24"
-
-		publicSubnet4Name := "Pulumi Public Subnet 4"
-		publicSubnet4Cidr := "10.0.12.0/24"
-		privateSubnet4AName := "Pulumi Private Subnet 4A"
-		privateSubnet4ACidr := "10.0.13.0/24"
-		privateSubnet4BName := "Pulumi Private Subnet 4B"
-		privateSubnet4BCidr := "10.0.17.0/24"
-
 		// Item 1
-		createPublicPrivateSubnets(ctx, namespace+"1", vpc, publicSubnet1Cidr, publicSubnet1Name, internetGateway, privateSubnet1ACidr, privateSubnet1AName, privateSubnet1BCidr, privateSubnet1BName, createPrivateSubnets, createAdditionalPrivateSubnets)
+		createPublicPrivateSubnets(ctx, namespace+"1", vpc, publicSubnet1Cidr, publicSubnet1Name, internetGateway, privateSubnet1ACidr, privateSubnet1AName, privateSubnet1BCidr, privateSubnet1BName, createPublicSubnets, createPrivateSubnets, createAdditionalPrivateSubnets, createNatGateways)
 
 		// Item 2
-		createPublicPrivateSubnets(ctx, namespace+"2", vpc, publicSubnet2Cidr, publicSubnet2Name, internetGateway, privateSubnet2ACidr, privateSubnet2AName, privateSubnet2BCidr, privateSubnet2BName, createPrivateSubnets, createAdditionalPrivateSubnets)
+		createPublicPrivateSubnets(ctx, namespace+"2", vpc, publicSubnet2Cidr, publicSubnet2Name, internetGateway, privateSubnet2ACidr, privateSubnet2AName, privateSubnet2BCidr, privateSubnet2BName, createPrivateSubnets, createPrivateSubnets, createAdditionalPrivateSubnets, createNatGateways)
 
-		// // Item 1
-		createPublicPrivateSubnets(ctx, namespace+"3", vpc, publicSubnet3Cidr, publicSubnet3Name, internetGateway, privateSubnet3ACidr, privateSubnet3AName, privateSubnet3BCidr, privateSubnet3BName, createPrivateSubnets, createAdditionalPrivateSubnets)
+		// Item 3
+		createPublicPrivateSubnets(ctx, namespace+"3", vpc, publicSubnet3Cidr, publicSubnet3Name, internetGateway, privateSubnet3ACidr, privateSubnet3AName, privateSubnet3BCidr, privateSubnet3BName, createPrivateSubnets, createPrivateSubnets, createAdditionalPrivateSubnets, createNatGateways)
 
-		// // Item 2
-		createPublicPrivateSubnets(ctx, namespace+"4", vpc, publicSubnet4Cidr, publicSubnet4Name, internetGateway, privateSubnet4ACidr, privateSubnet4AName, privateSubnet4BCidr, privateSubnet4BName, createPrivateSubnets, createAdditionalPrivateSubnets)
+		// Item 4
+		createPublicPrivateSubnets(ctx, namespace+"4", vpc, publicSubnet4Cidr, publicSubnet4Name, internetGateway, privateSubnet4ACidr, privateSubnet4AName, privateSubnet4BCidr, privateSubnet4BName, createPrivateSubnets, createPrivateSubnets, createAdditionalPrivateSubnets, createNatGateways)
 
 		return nil
 	})
@@ -100,7 +115,7 @@ func resourceName(namespace string, resourceName string) string {
 }
 
 // @fixme - this function includes a "DependsOn block" for the internet gateway
-func createPublicSubnet(ctx *pulumi.Context, namespace string, subnetCidr string, subnetName string, vpc *ec2.Vpc, internetGateway *ec2.InternetGateway) (*ec2.NatGateway, error) {
+func createPublicSubnet(ctx *pulumi.Context, namespace string, subnetCidr string, subnetName string, vpc *ec2.Vpc, internetGateway *ec2.InternetGateway, createNatGateways bool) (*ec2.NatGateway, error) {
 	natGatewayName := resourceName(namespace, "Pulumi NAT Gateway")
 	publicRouteTableName := resourceName(namespace, "Pulumi Public Route Table")
 
@@ -116,31 +131,6 @@ func createPublicSubnet(ctx *pulumi.Context, namespace string, subnetCidr string
 
 	if publicSubnetErr != nil {
 		return nil, publicSubnetErr
-	}
-
-	// NAT Gateway + Elastic IP
-	elasticIp, elasticIpErr := ec2.NewEip(ctx, resourceId(namespace, "elastic-ip"), &ec2.EipArgs{
-		Vpc: pulumi.Bool(true),
-	})
-
-	if elasticIpErr != nil {
-		return nil, elasticIpErr
-	}
-
-	// @fixme - return natGateway for the private subnet
-	natGateway, natGatewayErr := ec2.NewNatGateway(ctx, resourceId(namespace, "nat-gateway"), &ec2.NatGatewayArgs{
-		AllocationId: elasticIp.ID(),
-		SubnetId:     publicSubnet.ID(),
-		Tags: pulumi.StringMap{
-			"Name": pulumi.String(natGatewayName),
-		},
-		// Explicitly declare that this NAT Gateway depends on the Internet Gateway's deployment to be completed
-	}, pulumi.DependsOn([]pulumi.Resource{
-		internetGateway,
-	}))
-
-	if natGatewayErr != nil {
-		return nil, elasticIpErr
 	}
 
 	publicRouteTable, publicRouteTableErr := ec2.NewRouteTable(ctx, resourceId(namespace, "public-route-table"), &ec2.RouteTableArgs{
@@ -173,7 +163,37 @@ func createPublicSubnet(ctx *pulumi.Context, namespace string, subnetCidr string
 		return nil, publicRouteErr
 	}
 
-	return natGateway, nil
+	// @fixme - this could be moved to a separate function "createNatGateway(subnetId)"
+	if createNatGateways {
+		// NAT Gateway + Elastic IP
+		elasticIp, elasticIpErr := ec2.NewEip(ctx, resourceId(namespace, "elastic-ip"), &ec2.EipArgs{
+			Vpc: pulumi.Bool(true),
+		})
+
+		if elasticIpErr != nil {
+			return nil, elasticIpErr
+		}
+
+		// @fixme - return natGateway for the private subnet
+		natGateway, natGatewayErr := ec2.NewNatGateway(ctx, resourceId(namespace, "nat-gateway"), &ec2.NatGatewayArgs{
+			AllocationId: elasticIp.ID(),
+			SubnetId:     publicSubnet.ID(),
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String(natGatewayName),
+			},
+			// Explicitly declare that this NAT Gateway depends on the Internet Gateway's deployment to be completed
+		}, pulumi.DependsOn([]pulumi.Resource{
+			internetGateway,
+		}))
+
+		if natGatewayErr != nil {
+			return nil, elasticIpErr
+		}
+
+		return natGateway, nil
+	}
+
+	return nil, nil
 }
 
 func createPrivateSubnet(ctx *pulumi.Context, namespace string, subnetCidr string, subnetName string, vpc *ec2.Vpc, natGateway *ec2.NatGateway) error {
@@ -191,35 +211,37 @@ func createPrivateSubnet(ctx *pulumi.Context, namespace string, subnetCidr strin
 		return privateSubnetErr
 	}
 
-	privateRouteTableName := resourceName(namespace, "Pulumi Private Route Table")
-	privateRouteTable, privateRouteTableErr := ec2.NewRouteTable(ctx, resourceId(namespace, "private-route-table"), &ec2.RouteTableArgs{
-		VpcId: vpc.ID(),
-		Tags: pulumi.StringMap{
-			"Name": pulumi.String(privateRouteTableName),
-		},
-	})
+	if nil != natGateway {
+		privateRouteTableName := resourceName(namespace, "Pulumi Private Route Table")
+		privateRouteTable, privateRouteTableErr := ec2.NewRouteTable(ctx, resourceId(namespace, "private-route-table"), &ec2.RouteTableArgs{
+			VpcId: vpc.ID(),
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String(privateRouteTableName),
+			},
+		})
 
-	if privateRouteTableErr != nil {
-		return privateRouteTableErr
-	}
+		if privateRouteTableErr != nil {
+			return privateRouteTableErr
+		}
 
-	_, privateRouteErr := ec2.NewRoute(ctx, resourceId(namespace, "private-route"), &ec2.RouteArgs{
-		RouteTableId:         privateRouteTable.ID(),
-		DestinationCidrBlock: pulumi.String(entireInternetCidr),
-		NatGatewayId:         natGateway.ID(),
-	})
+		_, privateRouteErr := ec2.NewRoute(ctx, resourceId(namespace, "private-route"), &ec2.RouteArgs{
+			RouteTableId:         privateRouteTable.ID(),
+			DestinationCidrBlock: pulumi.String(entireInternetCidr),
+			NatGatewayId:         natGateway.ID(),
+		})
 
-	if privateRouteErr != nil {
-		return privateRouteErr
-	}
+		if privateRouteErr != nil {
+			return privateRouteErr
+		}
 
-	_, privateRouteTableAssociationErr := ec2.NewRouteTableAssociation(ctx, resourceId(namespace, "private-route-table-association"), &ec2.RouteTableAssociationArgs{
-		SubnetId:     privateSubnet.ID(),
-		RouteTableId: privateRouteTable.ID(),
-	})
+		_, privateRouteTableAssociationErr := ec2.NewRouteTableAssociation(ctx, resourceId(namespace, "private-route-table-association"), &ec2.RouteTableAssociationArgs{
+			SubnetId:     privateSubnet.ID(),
+			RouteTableId: privateRouteTable.ID(),
+		})
 
-	if privateRouteTableAssociationErr != nil {
-		return privateRouteTableAssociationErr
+		if privateRouteTableAssociationErr != nil {
+			return privateRouteTableAssociationErr
+		}
 	}
 
 	/***********************************************************
@@ -266,11 +288,17 @@ func createPrivateSubnet(ctx *pulumi.Context, namespace string, subnetCidr strin
 	return nil
 }
 
-func createPublicPrivateSubnets(ctx *pulumi.Context, namespace string, vpc *ec2.Vpc, publicSubnetCidr string, publicSubnetName string, internetGateway *ec2.InternetGateway, privateSubnetCidr string, privateSubnetName string, privateSubnetBCidr string, privateSubnetBName string, withPrivateSubnet bool, withAdditionalPrivateSubnet bool) error {
-	natGateway, createPublicSubnetErr := createPublicSubnet(ctx, resourceId(namespace, "public"), publicSubnetCidr, publicSubnetName, vpc, internetGateway)
+func createPublicPrivateSubnets(ctx *pulumi.Context, namespace string, vpc *ec2.Vpc, publicSubnetCidr string, publicSubnetName string, internetGateway *ec2.InternetGateway, privateSubnetCidr string, privateSubnetName string, privateSubnetBCidr string, privateSubnetBName string, withPublicSubnet bool, withPrivateSubnet bool, withAdditionalPrivateSubnet bool, createNatGateways bool) error {
 
-	if createPublicSubnetErr != nil {
-		return createPublicSubnetErr
+	var natGateway *ec2.NatGateway = nil
+
+	if withPublicSubnet {
+		var createPublicSubnetErr error = nil
+		natGateway, createPublicSubnetErr = createPublicSubnet(ctx, resourceId(namespace, "public"), publicSubnetCidr, publicSubnetName, vpc, internetGateway, createNatGateways)
+
+		if createPublicSubnetErr != nil {
+			return createPublicSubnetErr
+		}
 	}
 
 	if withPrivateSubnet {
